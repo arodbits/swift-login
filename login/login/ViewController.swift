23692e5ac34706fdbc682a3317b35ec153d69c86
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-03-27 17:21:35 -0400

    first commit

---
+++ login/login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  login
+//
+//  Created by Anthony Rodriguez on 3/27/15.
+//  Copyright (c) 2015 capitalofcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
