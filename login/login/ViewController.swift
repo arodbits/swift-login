bed5616b9bfc67753a2f20db9abe22add6aeb20d
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-03-28 18:20:13 -0400

    working with NSSessions and JSON

--- login/login/ViewController.swift
+++ login/login/ViewController.swift
@@ -15,29 +15,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = "http://httpbin.org/user-agent"
         
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA"
         let requestURL = NSURL(string: url)
-        
-        let sharedSession = NSURLSession.sharedSession()
-        let downloadTask = sharedSession.downloadTaskWithURL(requestURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
-            var urlContent = NSString(contentsOfURL: requestURL!, encoding: NSUTF8StringEncoding, error: nil)
+        let request = NSURLRequest(URL: requestURL!)
+        let session = NSURLSession.sharedSession()
+        let downloadTask = session.dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
             
-            println(urlContent)
+            let strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            var err: NSError?
+            let json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as NSDictionary?
+            
+//            println(json!)
+            
+            if(err != nil){
+                //println(err!.localizedDescription)
+            }
+            else {
+//                we still need to check wheather the json variable has a value using the 
+//                optional binding
+                if let parseJSON = json {
+                    println(parseJSON["results"]["address_components"])
+                }
+            
+            }
+            
         })
         downloadTask.resume()
-        
-
         
 
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        
-        
-    
-        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
