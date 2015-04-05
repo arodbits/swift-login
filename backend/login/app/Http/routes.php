de3e2a58e18755787faeaf901793b148d4327ec0
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-04-05 12:25:51 -0400

    Working on signup api

--- backend/login/app/Http/routes.php
+++ backend/login/app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('api/register', '');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
