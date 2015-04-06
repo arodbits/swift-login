98123bf5424f4db7e204eb64231d36456432027b
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-04-06 18:30:14 -0400

    Refactoring DRY

--- login/login/OAuthAuthProvider.swift
+++ login/login/OAuthAuthProvider.swift
@@ -20,13 +20,38 @@ struct Credentials {
     var password: String
 }
 
-class OAuthAuthProvider {
+
+
+
+struct OAuthTokenRequest {
     
-//  Define configuration variables
-    let baseUrl         = NSURL(string: "http://homestead.app/")
+    //  Define configuration variables
     let client_id       = "QldBvenYfVoOqGCT"
     let client_secret   = "rlLlSVoUBGJ37NM61zOuK1mvRBwKstmg"
     let grant_type      = "password"
+    
+    var bodyDataString: String {
+        return "grant_type=\(self.grant_type)&client_id=\(self.client_id)&client_secret=\(client_secret)&password=\(self.credentials.password)&username=\(self.credentials.password)"
+    }
+    
+    var credentials: Credentials
+    
+    init(credentials: Credentials){
+        self.credentials = credentials
+    }
+    
+    
+
+    
+    
+    
+}
+
+
+class OAuthAuthProvider {
+//  Base URL
+    let baseUrl         = NSURL(string: "http://homestead.app/")
+
 
 //  Asynchronous call, returning a hanlder
     func request(type: RequestType, request: NSMutableURLRequest, handler : ()?){
@@ -50,8 +75,19 @@ class OAuthAuthProvider {
 //        return handlerWithAccessToken
         
         let url = NSURL(string: "http://homestead.app/oauth/access_token", relativeToURL: self.baseUrl)
-        let request = 
-        self.request(type: get, request: <#NSMutableURLRequest#>, handler: <#()?#>)
+        let oauthTokenRequest = OAuthTokenRequest(credentials: credentials)
+        
+        self.request(type: RequestType.get, request: request, handler: {()->Void in
+        
+        })
         
     }
 }
+
+
+let request     = NSMutableURLRequest(URL: params.url)
+let username    = params.username
+let password    = params.password
+let bodyData    = "grant_type=\(self.grant_type)&username=\(username)&password=\(password)&client_id=\(self.client_id)&client_secret=\(self.client_secret)"
+request.HTTPMethod = "POST"
+request.HTTPBody
